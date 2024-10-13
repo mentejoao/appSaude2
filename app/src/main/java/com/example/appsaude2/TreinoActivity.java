c4d0d2e42c9e64af1bb8288d86a8fed9c86cdcc6
mentejoao <skhulzxd@gmail.com>
2024-10-13 17:48:01 -0300

    feat: adicionando TreinoActivity + .xml

--- app/src/main/java/com/example/appsaude2/TreinoActivity.java
+++ app/src/main/java/com/example/appsaude2/TreinoActivity.java
@@ -1,4 +1,12 @@
 package com.example.appsaude2;
 
-public class TreinoActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TreinoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_treino); // Crie um arquivo XML correspondente
+    }
 }
